e7d42f75e15ebf73c05ea028dad4d0482751ea94
Payden <69829508+Payden1224@users.noreply.github.com>
2023-05-10 14:30:21 -0500

    added bet per line

--- project.js
+++ project.js
@@ -10,7 +10,7 @@ const prompt = require("prompt-sync")();
 
 const deposit = () => {
     while (true) {
-        const depositAmount = prompt ("Enter a deposit amount: ");
+    const depositAmount = prompt ("Enter a deposit amount: ");
     const numberDepositAmount = parseFloat(depositAmount);
 
     if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
@@ -21,4 +21,32 @@ const deposit = () => {
     }
 };
 
-deposit();
+const getNumberOfLines = () => {
+    while (true) {
+        const Lines = prompt ("Enter the number of lines to bet on (1-3): ");
+        const numberOfLines = parseFloat(depositAmount);
+    
+        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+            console.log("Invalid number of lines, try again.")
+         } else {
+            return numberOfLines;
+         }
+        }
+};
+
+const getBet = (balance, lines) => {
+    while (true) {
+        const bet = prompt ("Enter the bet per line: ");
+        const numberBet = parseFloat(bet);
+    
+        if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+            console.log("Invalid bet, try again.");
+         } else {
+            return numberBet;
+         }
+        }
+};
+
+let balance = deposit();
+const numberOfLines = getNumberOfLines();
+const bet = getBet(balance, numberOfLines);
