d0dee6350763c1d757926d1e0eb4e6169816ca98
Payden <69829508+Payden1224@users.noreply.github.com>
2023-05-15 12:11:49 -0500

    print rows

--- project.js
+++ project.js
@@ -72,16 +72,45 @@ const spin = () => {
         }
 
     }
-        const reels = [[], [], []];
+        const reels = [];
         for (let i = 0; i < COLS; i++) {
+            reels.push([]);
             const reelSymbols = [...symbols];
             for (let j = 0; j < ROWS; j++) {
                 const randomIndex = Math.floor(Math.random() * reelSymbols.length)
                 const selectedSymbol = reelSymbols[randomindex];
+                reels[i].push(selectedSymbol);
+                reelsSymbols.splice(randomIndex, 1);
             }
         }
+
+        return reels;
 };
+
+const transpose = (reels) => {
+    const rows = [];
+
+    for (let i = 0; i < ROWS; i++) {
+        rows.push([]);
+        for (let j = 0; j < COLS; j++) {
+            rows[i].push(reels[j][i])
+        }
+    }
+
+    return rows
+};
+
+const printRows = (rows) => {
+    for (const row of rows) {
+        let rowString = "A";
+        for (const [i, symbol] of rows.entries()) {
+            rowString += symbol
+        }
+    }
+} 
 
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
+const reels = spin();
+const rows = transpose(reels);
